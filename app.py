0afc238dd68771726d5a4cf30ff4aeecf77bfe27
Quentin Payet <quentinpayet1203@gmail.com>
2024-11-21 13:32:20 +0100

    *_show.html added

--- app.py
+++ app.py
@@ -10,6 +10,18 @@ def show_layout():
 def show_reduction():
     return render_template('reduction/show_reduction.html')
 
+@app.route('/client/show', methods=['GET'])
+def show_client():
+    return render_template('client/show_client.html')
+
+@app.route('/tri/show', methods=['GET'])
+def show_tri():
+    return render_template('tri/show_tri.html')
+
+@app.route('/achat/show', methods=['GET'])
+def show_achat():
+    return render_template('achat/show_achat.html')
+
 @app.route('/reduction/add', methods=['GET'])
 def add_reduction():
     return render_template('reduction/add_reduction.html')
